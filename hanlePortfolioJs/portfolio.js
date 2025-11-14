d4cc1326e636c3bba92d87edde7d1ea1a9abcd3f
leehanle <leehanle12345@naver.com>
2025-11-14 17:50:29 +0900

    portfolio3

--- hanlePortfolioJs/portfolio.js
+++ hanlePortfolioJs/portfolio.js
@@ -1,7 +1,4 @@
-// $(function () {
-//   $("#visual .mainHanle").css({ transform: "translateX(-50%) scale(1)" }),
-//     $("#visual .text").css({ transform: "scale(1)" }, 1000);
-// });
+/*비주얼 구름 둥둥*/
 $(function () {
   $("#visual .mainHanle").css({
     transform: "translateX(-50%) scale(1)",
@@ -12,4 +9,17 @@ $(function () {
       transform: "scale(1)",
     });
   }, 1000);
+  /*문이 열리네용*/
+  $("#container .intro").on("mouseenter", function () {
+    $("#container .intro .wBox1").stop().animate({ left: "-1200px" }, 2000);
+  });
+  $("#container .intro").on("mouseleave", function () {
+    $("#container .intro .wBox1").stop().animate({ left: "0px" }, 2000);
+  });
+  $("#container .intro").on("mouseenter", function () {
+    $("#container .intro .wBox2").stop().animate({ right: "-1200px" }, 2000);
+  });
+  $("#container .intro").on("mouseleave", function () {
+    $("#container .intro .wBox2").stop().animate({ right: "0px" }, 2000);
+  });
 });
